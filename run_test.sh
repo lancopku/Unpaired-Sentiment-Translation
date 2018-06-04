6fea8f554eebf7ac70ae2d237099527275351d84
jingjingxupku <774716760@qq.com>
2018-06-04 12:23:37 +0800

    Add run file

--- run_test.sh
+++ run_test.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=2 nohup python run_summarization.py --mode='test' --data_path=train/* --vocab_path=vocab.txt --log_root=log --exp_name=myexperiment --gpuid=0
+python run_summarization.py --mode='test' --data_path=train/* --vocab_path=vocab.txt --log_root=log --exp_name=myexperiment --gpuid=0
